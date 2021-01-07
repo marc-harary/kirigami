05b9c029d86d0cc8ccd4467c6d1fbae5c302d2ef
Marc Harary <marcharary@vpn172022159110.its.yale.internal>
2021-01-07 01:02:25 -0500

    Refactored main network archiecture

--- kirigami/nn/MainNet.py
+++ kirigami/nn/MainNet.py
@@ -16,10 +16,15 @@ class MainNet(nn.Module):
             layer_obj = layer_class(**layer_dict['params'])
             setattr(self, f'layer{i}', layer_obj)
         self.n_layers = i
-    
+        
     def forward(self, input):
         ret = input
         for i in range(self.n_layers + 1):
             layer = getattr(self, f'layer{i}')
-            ret = layer(ret)
-        return ret 
+            if isinstance(layer, nn.LSTM):
+                ret = torch.transpose(ret, 1, 2)
+                ret, _ = layer(ret)
+                ret = torch.transpose(ret, 1, 2)
+            else:
+                ret = layer(ret)
+        return ret

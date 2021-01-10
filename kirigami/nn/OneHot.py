523f6c3024dc0710461eaeb8ee21b56f807d6422
Marc Harary <marcharary@Marcs-MacBook-Air.local>
2021-01-10 17:10:49 -0500

    Refactored embedding module

--- kirigami/nn/OneHot.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-class OneHot(nn.Module):
-    def forward(self, seq):
-        encodings = torch.stack([BASE_DICT[char] for char in seq.lower()])
-        return torch.unsqueeze(encodings, 2)

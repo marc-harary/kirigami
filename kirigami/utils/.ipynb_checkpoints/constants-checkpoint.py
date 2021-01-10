316e4ac27a66fab8d19e10d7e3f86384a055774e
Marc Harary <marcharary@Marcs-MacBook-Air.local>
2021-01-10 17:11:49 -0500

    Refactored embedding module

--- kirigami/utils/.ipynb_checkpoints/constants-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from collections import defaultdict
-import torch
-
-BASES = 'aucg'
-N_BASES = len(BASES)
-DEFAULT = -1 * torch.ones(N_BASES)
-BASE_DICT = defaultdict(lambda: DEFAULT, zip(BASES, torch.eye(N_BASES)))

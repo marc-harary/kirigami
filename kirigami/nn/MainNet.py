6832819d13cd9b1bee0f02c3fd92f544e20d211a
Marc Harary <mah258@farnam2.farnam.hpc.yale.internal>
2021-01-18 03:18:04 -0500

    Further work on training script

--- kirigami/nn/MainNet.py
+++ kirigami/nn/MainNet.py
@@ -7,7 +7,7 @@ class MainNet(nn.Module):
     def __init__(self, dict_list):
         super(MainNet, self).__init__()
         for i, layer_dict in enumerate(dict_list):
-            module_name, class_name = layer_dict['class'].rsplit('.', 1)
+            module_name, class_name = layer_dict['class_name'].rsplit('.', 1)
             module_ptr = modules[module_name]
             class_ptr = getattr(module_ptr, class_name)
             obj_ptr = class_ptr(**layer_dict['kwargs'])
--- kirigami/utils/data_utils.py
+++ kirigami/utils/data_utils.py
@@ -2,37 +2,30 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from ..nn.Embedding import *
 
-
 class AbstractDataset(Dataset):
-    def __init__(self, list_file: str, embed):
-        super(AbstractDataset, self).__init__()
-        self.embed = embed
-        with open(list_file, 'r') as f:
-            self.files = f.read.splitlines()
-
-    def __len__(self):
-        return len(self.files)
-
-    def __getitem__(self, idx):
-        file = self.files[idx]
-        with open(file, 'r') as f:
-            file_str = f.read()
-        return self.embed(file_str)
-
+	def __init__(self, list_file: str, embed: AbstractEmbedding):
+		super(AbstractDataset, self).__init__()
+		self.embed = embed
+		with open(list_file, 'r') as f:
+			self.files = f.read().splitlines()
+		
+	def __len__(self):
+		return len(self.files)
+	
+	def __getitem__(self, idx):
+		file = self.files[idx]
+		with open(file, 'r') as f:
+			file_str = f.read()
+		return self.embed(file_str)
 
 class FastaDataset(AbstractDataset):
-    def __init__(self, list_file: str):
-        super(FastaDataset, self).__init__(list_file)
-        self.embed = SequenceEmbedding()
-
+	def __init__(self, list_file: str):
+		super(FastaDataset, self).__init__(list_file, SequenceEmbedding())
 
 class LabelDataset(AbstractDataset):
-    def __init__(self, list_file: str):
-        super(FastaDataset, self).__init__(list_file)
-        self.embed = LabelEmbedding()
+	def __init__(self, list_file: str):
+		super(FastaDataset, self).__init__(list_file, LabelEmbedding())
 
-
-class BpseqDataset(Dataset):
-    def __init__(self, list_file: str):
-        super(FBpseqDataset, self).__init__(list_file)
-        self.embed = BpseqEmbedding()
+class BpseqDataset(AbstractDataset):
+	def __init__(self, list_file: str):
+		super(BpseqDataset, self).__init__(list_file, BpseqEmbedding())

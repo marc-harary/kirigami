488538a6db91f3a40accd5979215d2509ff4b134
Marc Harary <marcharary@vpn17225166132.its.yale.internal>
2021-01-17 21:30:16 -0500

    Added setup script and LICENSE

--- kirigami/scripts/train.py
+++ kirigami/scripts/train.py
@@ -1,13 +1,14 @@
 import argparse
 import os
 import json
+from tqdm import tqdm
 from munch import munchify
 import torch
 from torch import nn
 from torch.utils.data import *
-from ..nn.MainNet import *
-from ..nn.Embedding import *
-from ..utils.data_utils import *
+from kirigami.nn.MainNet import *
+from kirigami.nn.Embedding import *
+from kirigami.utils.data_utils import *
 
 
 def main():
@@ -23,7 +24,8 @@ def main():
     model = MainNet(conf.model)
     loss_func = getattr(nn, conf.loss_func.class)(**conf.loss_func.params)
     optimizer = getattr(nn, conf.optim.class)(model.parameters(),
-                                          **conf.optim.params)
+                                              **conf.optim.params)
+    start_epoch = 0
 
     train_set = BpseqDataset(conf.train_list)
     val_set = BpseqDataset(conf.validation_list)
@@ -34,20 +36,24 @@ def main():
                             batch_size=conf.training.batch_size,
                             shuffle=conf.training.shuffle)
 
-    if conf.resume and os.path.exists(conf.training.checkpoint):
+    if conf.resume:
+        assert os.path.exists(conf.training.checkpoint), "Cannot find checkpoint file"
         checkpoint = torch.load(conf.training.checkpoint)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        epoch = checkpoint['epoch']
+        start_epoch = checkpoint['epoch']
         loss = checkpoint['loss']
 
-    for epoch in range(conf.train.epochs):
+    for epoch in range(start_epoch, conf.train.epochs):
+        pbar = tqdm(total=len(train_loader))
         for seq, label in train_loader:
             pred = model(seq)
             loss = loss_func(pred, label)
             loss.backward()
             optimzer.step()
             optimizer.zero_grad()
+            pbar.update(1)
+        pbar.close()
         torch.save({'epoch': epoch,
                     'model_state_dict': model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict(),

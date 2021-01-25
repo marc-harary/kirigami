b826330b4fc98faed0f718d8e4f70e107e8e73f2
Marc Harary <marcharary@vpn17225166132.its.yale.internal>
2021-01-25 01:46:43 -0500

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='kirigami',
       version='1.0',
       description='RNA secondary structure prediction via deep learning',
       author='Marc Harary',
       author_email='marc.harary@yale.edu',
-      packages=['kirigami']
+      packages=find_packages()
      )

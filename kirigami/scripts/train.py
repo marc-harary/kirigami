fe99a405430c05f9f5b851c5d4e8deeeb8feabd5
Marc Harary <marcharary@vpn17225166132.its.yale.internal>
2021-01-30 16:28:13 -0500

    Refactored dataset

--- kirigami/scripts/train.py
+++ kirigami/scripts/train.py
@@ -6,12 +6,11 @@ from multipledispatch import dispatch
 from tqdm import tqdm
 import munch
 import torch
-from torch import nn
+from torch.optim import *
+from torch.nn import *
 from torch.utils.data import DataLoader
-from kirigami.utils.utils import path2munch
-from kirigami.utils.data_utils import *
-from kirigami.nn.SPOT import *
-from kirigami.nn.Embedding import *
+from kirigami.utils.utilities import path2munch
+from kirigami.utils.data import *
 from kirigami.nn.MainNet import *
 
 
@@ -21,54 +20,32 @@ __all__ = ['train']
 @dispatch(argparse.Namespace)
 def train(args) -> None:
     config = path2munch(args.config)
-    return train(config, args.quiet)
+    return train(config, args.quiet, args.resume)
 
 
 @dispatch(munch.Munch, bool)
-def train(config, quiet) -> None:
+def train(config, quiet=True, resume=False) -> None:
     '''Train deep network based on config files'''
     start_epoch = 0
     model = MainNet(config.model)
-    loss_func = getattr(nn, config.loss_func.class_name)(**config.loss_func.params)
-    optimizer = getattr(torch.optim, config.optim.class_name)(model.parameters(),
-                                                            **config.optim.params)
-    
-    fp = tempfile.TemporaryFile()
-    with open(config.data.training_list, 'r') as f:
-        files = f.read().splitlines() 
-
-    for file in train_bpseq:
-        with open(file, 'r') as f:
-            file = f.read()
-        file_embed = bpseq2tensor(file)
-        file = os.path.basename(file)
-        file, _ = os.path.splitext(file)
-        file = os.path.join(out_dir, file)
-        file += '.pt'
-        torch.save(file_embed, file) 
-    
-        train_bpseq_dataset.embed(config.data.embedding_directory, config.data.embedding_list)
-
-    train_set = TensorDataset(config.data.embedding_list):
+    loss_func = eval(config.loss_func.class_name)(**config.loss_func.params)
+    optimizer = eval(config.optim.class_name)(model.parameters(), **config.optim.params)
 
     best_val_loss = float('inf')
-    dataset_class = TensorDataset if config.data.pre_embed else BpseqDataset
-    train_set = dataset_class(config.data.training_list)
+    train_set = BpseqDataset(config.data.training_list, quiet)
     train_loader = DataLoader(train_set,
                               batch_size=config.data.batch_size,
                               shuffle=config.data.shuffle)
-    train_loop = tqdm(train_loader) if not quiet else train_loader
-
+    train_loop = train_loader if quiet else tqdm(train_loader)
 
     if config.data.validation_list:
-        val_set = dataset_class(config.data.validation_list)
+        val_set = BpseqDataset(config.data.validation_list, quiet)
         val_loader = DataLoader(val_set,
                                 batch_size=config.data.batch_size,
                                 shuffle=config.data.shuffle)
-        val_loop = tqdm(val_loader) if not quiet else val_loader
+        val_loop = val_loader if quiet else tqdm(val_loader)
 
-
-    if config.resume:
+    if resume:
         assert os.path.exists(config.training.best), "Cannot find checkpoint file"
         best = torch.load(config.training.best)
         model.load_state_dict(best['model_state_dict'])
@@ -76,7 +53,6 @@ def train(config, quiet) -> None:
         start_epoch = best['epoch']
         loss = best['loss']
         best_val_loss = best['best_val_loss']
-
 
     for epoch in range(start_epoch, config.training.epochs):
         train_loss_tot = 0.
@@ -100,7 +76,7 @@ def train(config, quiet) -> None:
                 pred = model(seq)
                 loss = loss_func(pred, lab)
                 val_loss_tot += loss
-                val_loss_mean = val_loss_tot / len(val_loader)
+            val_loss_mean = val_loss_tot / len(val_loader)
             if val_loss_mean < best_val_loss:
                 best_val_loss = val_loss_mean
                 torch.save({'epoch': epoch,
--- kirigami/utils/data.py
+++ kirigami/utils/data.py
@@ -1,58 +1,48 @@
 from pathlib import Path
 from typing import Callable
 import os
+from tqdm import tqdm
 import torch
 from torch.utils.data import Dataset
 from kirigami.utils.utilities import *
 
 
-__all__ = ['TensorDataset',
-           'AbstractASCIIDataset',
+__all__ = ['AbstractASCIIDataset',
            'FastaDataset',
            'LabelDataset',
            'BpseqDataset']
 
 
-class TensorDataset(Dataset):
-    def __init__(self, list_file: Path):
-        super(TensorDataset, self).__init__()
-        with open(list_file, 'r') as f:
-            self.files = f.read().splitlines()
-
-    def __len__(self):
-        return len(self.files)
-
-    def __getitem__(self, idx: int):
-        return torch.load(self.files[idx])
-
-
 class AbstractASCIIDataset(Dataset):
-    def __init__(self, list_file: Path, embedding: Callable) -> None:
+    def __init__(self, list_file: Path, embedding: Callable, quiet: bool = False) -> None:
         super(AbstractASCIIDataset, self).__init__()
-        self.embedding = embedding
         with open(list_file, 'r') as f:
-            self.files = f.read().splitlines()
+            files = f.read().splitlines()
+        self.data = []
+        print('Embedding files...')
+        loop = files if quiet else tqdm(files)
+        for file in loop:
+            with open(file, 'r') as f:
+                txt = f.read()
+            self.data.append(embedding(txt))
 
     def __len__(self):
-        return len(self.files)
+        return len(self.data)
 
     def __getitem__(self, idx: int):
-        file = self.files[idx]
-        with open(file, 'r') as f:
-            file_str = f.read()
-        return self.embedding(file_str)
+        return self.data[idx]
 
 
 class FastaDataset(AbstractASCIIDataset):
-    def __init__(self, list_file: str):
-        super(FastaDataset, self).__init__(list_file, sequence2tensor)
+    def __init__(self, list_file: Path, quiet: bool = False) -> None:
+        super(FastaDataset, self).__init__(list_file, sequence2tensor, quiet)
 
 
 class LabelDataset(AbstractASCIIDataset):
-    def __init__(self, list_file: str):
-        super(FastaDataset, self).__init__(list_file, label2tensor)
+    def __init__(self, list_file: Path, quiet: bool = False) -> None:
+        super(FastaDataset, self).__init__(list_file, label2tensor, quiet)
 
 
 class BpseqDataset(AbstractASCIIDataset):
-    def __init__(self, list_file: str):
-        super(BpseqDataset, self).__init__(list_file, bpseq2tensor)
+    def __init__(self, list_file: Path, quiet: bool = False) -> None:
+        super(BpseqDataset, self).__init__(list_file, bpseq2tensor, quiet)

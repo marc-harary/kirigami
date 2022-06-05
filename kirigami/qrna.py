0c33d789f70034e932d2c6fefb608fdfad86209b
marc-harary <marc.harary@yale.edu>
2022-06-05 17:55:27 -0400

    Fixed inplace bug

--- kirigami/qrna.py
+++ kirigami/qrna.py
@@ -23,15 +23,17 @@ class QRNABlock(nn.Module):
                                      dilation=dilations[0],
                                      padding=self.get_padding(dilations[0], kernel_sizes[0]))
         self.norm1 = torch.nn.InstanceNorm2d(n_channels)
-        self.relu1 = torch.nn.ReLU(inplace=True)
-        self.drop1 = torch.nn.Dropout(p=p, inplace=True)
+        self.relu1 = torch.nn.ReLU()
+        self.drop1 = torch.nn.Dropout(p=p)
+        # self.relu1 = torch.nn.ReLU(inplace=True)
+        # self.drop1 = torch.nn.Dropout(p=p, inplace=True)
         self.conv2 = torch.nn.Conv2d(in_channels=n_channels,
                                      out_channels=n_channels,
                                      kernel_size=kernel_sizes[1],
                                      dilation=dilations[1],
                                      padding=self.get_padding(dilations[1], kernel_sizes[1]))
         self.norm2 = torch.nn.InstanceNorm2d(n_channels)
-        self.relu2 = torch.nn.ReLU(inplace=True)
+        self.relu2 = torch.nn.ReLU()
 
     def forward(self, ipt: torch.Tensor) -> torch.Tensor:
         out = ipt
@@ -88,18 +90,31 @@ class QRNANet(nn.Module):
                               resnet=True)
             setattr(self, f"block{i}", block)
 
-        self.con_conv = nn.Conv2d(in_channels=self.n_channels,
-                                  out_channels=1,
-                                  kernel_size=self.fork_kernel_size,
-                                  padding=self.fork_padding)
-        self.bin_conv = nn.Conv3d(in_channels=self.n_channels,
-                                  out_channels=self.n_bins,
-                                  kernel_size=(1, self.fork_kernel_size, self.fork_kernel_size),
-                                  padding=(0, self.fork_padding, self.fork_padding))
-        self.inv_conv = nn.Conv2d(in_channels=self.n_channels,
-                                  out_channels=1,
-                                  kernel_size=self.fork_kernel_size,
-                                  padding=self.fork_padding)
+        # self.con_conv = nn.Conv2d(in_channels=self.n_channels,
+        #                           out_channels=1,
+        #                           kernel_size=self.fork_kernel_size,
+        #                           padding=self.fork_padding)
+        # self.bin_conv = nn.Conv3d(in_channels=self.n_channels,
+        #                           out_channels=self.n_bins,
+        #                           kernel_size=(1, self.fork_kernel_size, self.fork_kernel_size),
+        #                           padding=(0, self.fork_padding, self.fork_padding))
+        # self.inv_conv = nn.Conv2d(in_channels=self.n_channels,
+        #                           out_channels=1,
+        #                           kernel_size=self.fork_kernel_size,
+        #                           padding=self.fork_padding)
+
+    # def forward(self, ipt):
+    #     opt = ipt
+    #     opt = self.conv_init(opt)
+    #     for i in range(self.n_blocks):
+    #         block = getattr(self, f"block{i}")
+    #         opt = block(opt)
+    #     opt_con = self.con_conv(opt).sigmoid()
+    #     opt_bin = opt.unsqueeze(-3)
+    #     opt_bin = opt_bin.expand(-1, -1, self.n_dists, -1, -1)
+    #     opt_bin = self.bin_conv(opt_bin)
+    #     opt_inv = self.inv_conv(opt).sigmoid()
+    #     return opt_con, opt_bin, opt_inv
 
     def forward(self, ipt):
         opt = ipt
@@ -107,10 +122,4 @@ class QRNANet(nn.Module):
         for i in range(self.n_blocks):
             block = getattr(self, f"block{i}")
             opt = block(opt)
-        opt_con = self.con_conv(opt).sigmoid()
-        opt_bin = opt.unsqueeze(-3)
-        opt_bin = opt_bin.expand(-1, -1, self.n_dists, -1, -1)
-        opt_bin = self.bin_conv(opt_bin)
-        opt_inv = self.inv_conv(opt).sigmoid()
-        return opt_con, opt_bin, opt_inv
-
+        return opt

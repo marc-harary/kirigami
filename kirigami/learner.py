9a960c8f7dc681a67361666da8908ea4e9b76510
marc-harary <marc.harary@yale.edu>
2022-06-05 23:37:31 -0400

    Obtains .68

--- kirigami/learner.py
+++ kirigami/learner.py
@@ -4,6 +4,7 @@ from torch import optim
 import torch.nn.functional as F
 import pytorch_lightning as pl
 import torchmetrics
+from kirigami.metrics import GroundMCC
 
 
 class KirigamiModule(pl.LightningModule):
@@ -15,18 +16,19 @@ class KirigamiModule(pl.LightningModule):
         super().__init__()
         self.net = net
         self.crit = crit
-        self.val_metrics = {}
-        self.val_metrics["con"] = torchmetrics.MatthewsCorrCoef(num_classes=2)
-        self.val_metrics["dists"] = {}
-        for dist_type in self.dist_types:
-            self.val_metrics["dists"][dist_type] = {}
-            self.val_metrics["dists"][dist_type]["bin"] = {}
-            self.val_metrics["dists"][dist_type]["inv"] = {}
-            for i in self.Ls:
-                self.val_metrics["dists"][dist_type]["bin"][f"pcc{i}"] = torchmetrics.PearsonCorrCoef()
-                self.val_metrics["dists"][dist_type]["bin"][f"mae{i}"] = torchmetrics.PearsonCorrCoef()
-                self.val_metrics["dists"][dist_type]["inv"][f"pcc{i}"] = torchmetrics.MeanAbsoluteError()
-                self.val_metrics["dists"][dist_type]["inv"][f"mae{i}"] = torchmetrics.MeanAbsoluteError()
+        # self.val_metrics = {}
+        self.mcc = GroundMCC()
+        # self.val_metrics["con"] = torchmetrics.MatthewsCorrCoef(num_classes=2)
+        # self.val_metrics["dists"] = {}
+        # for dist_type in self.dist_types:
+        #     self.val_metrics["dists"][dist_type] = {}
+        #     self.val_metrics["dists"][dist_type]["bin"] = {}
+        #     self.val_metrics["dists"][dist_type]["inv"] = {}
+        #     for i in self.Ls:
+        #         self.val_metrics["dists"][dist_type]["bin"][f"pcc{i}"] = torchmetrics.PearsonCorrCoef()
+        #         self.val_metrics["dists"][dist_type]["bin"][f"mae{i}"] = torchmetrics.PearsonCorrCoef()
+        #         self.val_metrics["dists"][dist_type]["inv"][f"pcc{i}"] = torchmetrics.MeanAbsoluteError()
+        #         self.val_metrics["dists"][dist_type]["inv"][f"mae{i}"] = torchmetrics.MeanAbsoluteError()
         
 
     def training_step(self, batch, batch_idx):
@@ -36,12 +38,10 @@ class KirigamiModule(pl.LightningModule):
         self.log("train_tot_loss",
                  loss_dict["tot"],
                  on_epoch=True,
-                 prog_bar=True,
                  logger=True)
         self.log("train_con_loss",
                  loss_dict["con"],
                  on_epoch=True,
-                 prog_bar=True,
                  logger=True)
         return loss_dict["tot"]
 
@@ -50,6 +50,11 @@ class KirigamiModule(pl.LightningModule):
         feat, lab_grd = batch
         lab_prd = self.net(feat)
         loss_dict = self.crit(lab_prd, lab_grd)
+        self.mcc(lab_prd, lab_grd, feat)
+        self.log("val_tot_loss", loss_dict["tot"], on_epoch=True, logger=True)
+        self.log("val_con_loss", loss_dict["con"], on_epoch=True, logger=True)
+        self.log("val_mcc", self.mcc, on_epoch=True, logger=True)
+        self.logger.log_image(key=f"mol{batch_idx:02}", images=[lab_grd["con"].squeeze(), lab_prd["con"].squeeze()])
         return loss_dict["tot"]
 
 
--- kirigami/metrics.py
+++ kirigami/metrics.py
@@ -7,24 +7,25 @@ class GroundMCC(Metric):
 
 
     _pairs = {"AU", "UA", "CG", "GC", "GU", "UG"}
+    _min_dist = 4
 
 
     def __init__(self):
         super().__init__()
-        self.add_state("tp", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("tn", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("fp", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("fn", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("n_prd", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("n_grd", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("tp", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("tn", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("fp", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("fn", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("n_prd", default=torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("n_grd", default=torch.tensor(0.), dist_reduce_fx="sum")
 
 
     # def update(self, prd_pairs: set, grd_pairs: set, seq_len: int) -> dict:
-    def update(self, prd: torch.Tensor, grd: torch.Tensor):
-        seq = self._fasta2str(grd)
+    def update(self, prd: dict, grd: dict, feat: torch.Tensor):
+        seq = self._fasta2str(feat)
         seq_len = len(seq)
-        prd_pairs = self._prd2set(prd)
         grd_pairs = self._grd2set(grd)
+        prd_pairs = self._prd2set(prd, len(grd_pairs), seq)
 
         total = seq_len * (seq_len-1) / 2
         tp = float(len(prd_pairs.intersection(grd_pairs)))
@@ -55,10 +56,7 @@ class GroundMCC(Metric):
 
     def _grd2set(self, ipt: torch.Tensor):
         """Converts ground truth contact `Tensor` to set of ordered pairs"""
-        if isinstance(ipt, Tuple):
-            ipt_ = ipt[0]
-        else:
-            ipt_ = ipt 
+        ipt_ = ipt["con"] 
         ipt_ = ipt_.squeeze()
         idxs = torch.where(ipt_) 
         idxs = torch.vstack(idxs).T
@@ -71,10 +69,7 @@ class GroundMCC(Metric):
 
     def _fasta2str(self, ipt: torch.Tensor) -> str:
         """Converts `Tensor` to FASTA string"""
-        if isinstance(ipt, Tuple):
-            ipt_ = ipt[0]
-        else:
-            ipt_ = ipt 
+        ipt_ = ipt 
         ipt_ = ipt.squeeze()[:4,:,0].char()
         total_length = ipt_.shape[1]
         fasta_length = int(ipt_.sum().item())
@@ -84,10 +79,7 @@ class GroundMCC(Metric):
 
     def _prd2set(self, ipt: torch.Tensor, thres_pairs: int, seq: str):
         """Converts predicted contact `Tensor` to set of ordered pairs"""
-        if isinstance(ipt, Tuple):
-            ipt_ = ipt[0]
-        else:
-            ipt_ = ipt 
+        ipt_ = ipt["con"] 
         ipt_ = ipt_.squeeze() 
         # get indices sorted in descending order by probability
         ipt_ = ipt_.flatten() # PyTorch makes sorting 2D tensors impractical
@@ -101,10 +93,10 @@ class GroundMCC(Metric):
         for (i, j) in zip(ii, jj):
             if num_pairs == thres_pairs:
                 break
-            if (seq[i]+seq[j] in self.PAIRS and # canonical base pairs
+            if (seq[i]+seq[j] in self._pairs and # canonical base pairs
                 not kept[i] and not kept[j] and # not already
                 i < j and # get upper triangular matrix
-                j - i >= min_dist): # ensure i and j are at least min_dist apart
+                j - i >= self._min_dist): # ensure i and j are at least min_dist apart
                     out_set.add((i.item(), j.item()))
                     kept[i] = kept[j] = True
                     num_pairs += 1

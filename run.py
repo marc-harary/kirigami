65142cfe5cd011d1cf0c041ae460de375a1acf3b
marc-harary <marc.harary@yale.edu>
2023-03-05 13:45:16 -0500

    Simplified some more

--- run.py
+++ run.py
@@ -1,14 +1,8 @@
-import os
 from pytorch_lightning.cli import LightningCLI
-# from kirigami.learner import KirigamiModule
 import kirigami
-from kirigami.data import DataModule
-# from kirigami.loss import ForkLoss
-# from kirigami.resnet import ResNetParallel
 
 def main():
     cli = LightningCLI(kirigami.KirigamiModule, kirigami.DataModule)
 
 if __name__ == "__main__":
     main()
-

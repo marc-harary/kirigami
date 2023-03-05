4a6a929bc3d020b4af788059cda82b329f4266b8
marc-harary <marc.harary@yale.edu>
2023-03-05 13:44:23 -0500

    Simplified API

--- kirigami/__init__.py
+++ kirigami/__init__.py
@@ -1,2 +1,4 @@
 # from . import nn
 # from . import utils
+from kirigami.learner import KirigamiModule
+from kirigami.data import DataModule
--- run.py
+++ run.py
@@ -1,36 +1,13 @@
-# import sys
 import os
-# from pathlib import Path
-# import math
-
-# import torch
-# import torch.nn as nn
-# 
-# import pytorch_lightning as pl
-# from pytorch_lightning.loggers import WandbLogger
-# from pytorch_lightning.plugins.environments import SLURMEnvironment
-# from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, StochasticWeightAveraging
-# 
 from pytorch_lightning.cli import LightningCLI
-# 
-# from argparse import ArgumentParser
-# 
-import wandb
-
+# from kirigami.learner import KirigamiModule
+import kirigami
 from kirigami.data import DataModule
-from kirigami.loss import ForkLoss
-from kirigami.learner import KirigamiModule
-from kirigami.resnet import ResNetParallel
-
-
-# feat_choices = {"pf", "pfold", "petfold", "pfile", "rnafold", "prob_pair",
-#                 "centroid", "subopt0", "subopt1", "subopt2", "subopt3"} 
-# dist_choices = ("PP", "O5O5", "C5C5", "C4C4", "C3C3", "C2C2", "C1C1", "O4O4", "O3O3", "NN")
-
+# from kirigami.loss import ForkLoss
+# from kirigami.resnet import ResNetParallel
 
 def main():
-    os.environ["WANDB_MODE"] = "offline"
-    cli = LightningCLI(KirigamiModule, DataModule)
+    cli = LightningCLI(kirigami.KirigamiModule, kirigami.DataModule)
 
 if __name__ == "__main__":
     main()

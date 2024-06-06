67623bbc7ce4949c3b17b979e2bd1840f3a69d77
Marc Harary <marcharary@marcs-mbp.dfci.harvard.edu>
2024-06-06 17:22:25 -0400

    Fixed bugs in torch hub utilities

--- hubconf.py
+++ hubconf.py
@@ -5,6 +5,9 @@ dependencies = ['torch']
 from kirigami import KirigamiModule
 
 def kirigami(pretrained=True, checkpoint_path='weights/main.ckpt'):
+    hub_dir = os.path.dirname(os.path.abspath(__file__))
+    # Construct the full path to the checkpoint file
+    checkpoint_path = os.path.join(hub_dir, checkpoint_path)
     model = KirigamiModule.load_from_checkpoint(checkpoint_path)
     model.eval()
     return model
--- kirigami/learner.py
+++ kirigami/learner.py
@@ -8,67 +8,10 @@ from kirigami.layers import ResNet, Greedy
 from kirigami.constants import GRID
 from kirigami.utils import get_con_metrics
 import kirigami
+from kirigami.utils import _embed_fasta, mat2db
 
 
 class KirigamiModule(pl.LightningModule):
-
-    """
-    Inherits from `pytorch_lightning.LightningModule` to create wrapper class
-    for Kirigami network. Serves as main API to Kirigami pipeline.
-
-    Attributes
-    ----------
-    threshold : float
-        Pass
-    optim : str
-        Name of `torch.optim.Optimizer` object to be instantiated for training
-        by `configure_optimizers`.
-    lr : float
-        Learning rate for training.
-    model : kirigami.layers.ResNet
-        Residual neural network module.
-    crit : nn.Module
-        Criterion for computing binary cross-entropy loss.
-    post_proc : kirigami.layers.Greedy
-        Post-processing module for enforcing constraints on output.
-    raw_val_metrics : dict
-        Dictionary of classification metrics for non-processed predicted
-        labels on validation set as computed at each threshold in grid search.
-    proc_val_metrics : dict
-        Dictionary of classification metrics for post-processed predicted
-        labels on validation set as computed at each threshold in grid search.
-    n_val : int
-        Number of molecules in validation set.
-    test_rows : list
-        List containing dictionary of classification metrics for each molecule
-        in test set.
-
-    Methods
-    -------
-    configure_optimizers()
-        Sets up optimizer for training.
-    on_fit_start()
-        Logs code for Weights and Biases at start of training.
-    training_step(batch, batch_idx)
-        Script for step of training epoch.
-    on_validation_epoch_start()
-        Reinitializes validation metric caches for grid search at start of
-        validation epoch.
-    validation_step(batch, batch_idx)
-        Script for step of validation epoch.
-    on_validation_epoch_end()
-        Perform grid search to determine optimal threshold for classification
-        metrics.
-    on_test_epoch_start()
-        Reinitialize table of classification metrics for test set.
-    test_step(batch, batch_idx)
-        Script for step of test epoch.
-    on_test_epoch_end
-        Computes mean classification metrics at end of test epoch.
-    forward(feat, post_proc=True)
-        Performs single prediction step for input primary structure.
-    """
-
     def __init__(
         self,
         n_blocks: int,
@@ -130,7 +73,7 @@ class KirigamiModule(pl.LightningModule):
         feat, grd = batch
         prd = self.model(feat)
         prd = self.post_proc(prd, feat)
-        loss = self.crit(prd, grd)
+        loss = F.binary_cross_entropy(prd, grd)
         self.log("test/loss", loss)
         for i, val in enumerate(GRID):
             metrics_dict = get_con_metrics(prd, grd, val.item())
@@ -145,5 +88,20 @@ class KirigamiModule(pl.LightningModule):
         prd = self.model(feat)
         if post_proc:
             prd = self.post_proc(prd, feat)
-        # prd[prd < self.threshold.item()] = 0
+        prd[prd < self.threshold.item()] = 0
         return prd
+
+    def __call__(self, seq):
+        fasta = _embed_fasta(seq.upper())
+        fasta = fasta[..., None]
+        fasta = torch.cat(fasta.shape[-2] * [fasta], dim=-1)
+        fasta_t = fasta.transpose(-1, -2)
+        fasta = torch.cat([fasta, fasta_t], dim=-3)
+        fasta = fasta[None, ...]
+        fasta = fasta.float()
+
+        prd = self.forward(fasta)
+
+        dbn = mat2db(prd)
+
+        return dbn
